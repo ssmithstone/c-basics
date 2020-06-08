b924bae6df22751aabb5b8d5380498e3b0f0ff33
Stephen Smithstone <stephen.smithstone@digitaslbi.com>
2020-06-08 19:58:01 +1000

    added locale, to use system locale setup using LC_ALL=en_AU.UTF-8

--- src/main.c
+++ src/main.c
@@ -1,9 +1,34 @@
 #include <stdio.h>
+#include <locale.h>
 
 int main(){
 
 	printf("%s\n" , "Learning Programming Lanaguges");
 	printf("%s\n" , "c-basics");
+
+
+	int a = 10;
+	float b = 10.0;
+	char c = 'c';
+	char * string = "hello world";
+	int * ptr = &a;
+
+	printf("%s a char string\n" , string);
+	printf("%d an int\n" , a);
+	printf("%f a float\n" , b);
+	printf("%.2f a float to 2 decimal places\n" , b);
+	printf("%c a char\n" , c);
+	printf("%p a pointer\n" , ptr);
+	printf("%d int pointer deferenced\n" , *ptr);
+
+	setlocale(LC_ALL, "");
+	struct lconv * l = localeconv();
+
+	printf("decimal point of locale = %s\n", l->decimal_point);
+	printf("thousands sep of locale = %s\n", (*l).thousands_sep);
+	printf("int_curr_symbol of locale = %s\n", (*l).int_curr_symbol);
+	printf("positive_sign of locale = %s\n", (*l).positive_sign);
+	printf("negative_sign of locale = %s\n", (*l).negative_sign);
 
 	return 0;
 }
